029dfad70974759a892c1253e263c93df4167e9c
kielagustin1 <kielagustin16@gmail.com>
2024-11-05 04:48:38 +0800

    Initial Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Link } from 'svelte-routing';
+</script>
+
+<main
+  class="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
+  style="background-image: url('https://e0.pxfuel.com/wallpapers/475/184/desktop-wallpaper-portfolio-background-portfolio-background-portfolio-and-career-portfolio.jpg'); margin: 0;"
+>
+  <h1 class="text-6xl text-white">Portfolio</h1>
+  <p class="mt-4 text-xl text-white">Kiel Agustin</p>
+  <p class="mt-4 text-2xl text-white">BSCS 3C</p>
+  <Link to="/portfolio" class="text-white rounded-full shadow-lg transition duration-300">
+    Portfolio
+  </Link>
+</main>
